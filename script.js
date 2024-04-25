4d765f9b97374cde517b946436a0d698033bb1b8
Santanu064 <santanughosh.keunda@gmail.com>
2024-04-25 21:40:36 +0530

    add a card carouler

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
-function loginPage() {
+const login_btn = document.querySelector(".Account")
+login_btn.addEventListener('click',() => {
     window.location.href = "Login_Page.html";
-}
+});
+
+const Left_btn = document.querySelector(".l_btn")
+const Right_btn = document.querySelector(".r_btn");
+
+Right_btn.addEventListener('click' , (e)=>{
+    console.log("done");
+    const slide = document.querySelector(".card_silder")
+    slide.scrollLeft+=1200;
+    e.preventDefault();
+})
+Left_btn.addEventListener("click", (e) => {
+  console.log("done");
+  const slide = document.querySelector(".card_silder");
+  slide.scrollLeft -= 1100;
+  e.preventDefault();
+});
