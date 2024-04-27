2803bae2a1e924873682e0076386776c682a030e
Santanu064 <santanughosh.keunda@gmail.com>
2024-04-27 22:54:29 +0530

    add drawer functionanity

--- script.js
+++ script.js
@@ -3,17 +3,20 @@ login_btn.addEventListener('click',() => {
     window.location.href = "Login_Page.html";
 });
 
+const drawer_login = document.querySelector("#login");
+drawer_login.addEventListener("click", () => {
+  window.location.href = "Login_Page.html";
+});
+
 const Left_btn = document.querySelector(".l_btn")
 const Right_btn = document.querySelector(".r_btn");
 
 Right_btn.addEventListener('click' , (e)=>{
-    console.log("done");
     const slide = document.querySelector(".card_silder")
     slide.scrollLeft+=1200;
     e.preventDefault();
 })
 Left_btn.addEventListener("click", (e) => {
-  console.log("done");
   const slide = document.querySelector(".card_silder");
   slide.scrollLeft -= 1100;
   e.preventDefault();
